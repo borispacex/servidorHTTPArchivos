7d64104c2961aaeda84b85aadca6ea2a4b48593e
Boris Vargas <borisvargaspaucara@gmail.com>
2020-06-19 20:37:44 -0400

    ya probado

--- servidor.js
+++ servidor.js
@@ -4,29 +4,22 @@ var fs = require('fs');     // modulo para ver archivos
 
 // crear el servidor http
 var servidor = http.createServer(function(request, response) { // request: recibir
-    /* OBTENEMOS LA DATA MEDIANTE EL METODO get */
     if (request.method == 'GET') {
-        console.log(request.url);   //    /doc/test2.json
-        // JSON
-        if (request.url === '/doc/test2.json') {
-            var json = fs.readFileSync('./doc/test2.json');
+        var uri = url.parse(request.url).pathname;
+        console.log(uri);               // /doc/test2.json
+        if (fs.existsSync('.' + uri)) {                 // si existe ./doc/test2.json
+            console.log('Encontrado....');
+            var archivo = fs.readFileSync('.' + uri);   //  ./doc/test2.json
             response.writeHead(200, {'Content-Type': 'text/plain'});
-            response.end(json,'binary');
-            return;
-        }
-        // HTML
-        if (request.url === '/pub/test2.html') {
-            var html = fs.readFileSync('./pub/test2.html');
-            response.writeHead(200, {'Content-Type': 'text/plain'});
-            response.end(html,'binary');
+            response.end(archivo,'binary');
             return;
         } else {
+            console.log('NO encontrado....');
             response.writeHead(404, {"Content-Type": "text/plain"});
             response.write("404 Not Found\n");
             response.end();
             return;
         }
-
     }
 });
 
