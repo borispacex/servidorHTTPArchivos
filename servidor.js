87e155e7f10dc434563c0f28c7443e150d793d36
Boris Vargas <borisvargaspaucara@gmail.com>
2020-06-19 20:50:28 -0400

    subiendo archivos pdf iamgen

--- servidor.js
+++ servidor.js
@@ -6,12 +6,12 @@ var fs = require('fs');     // modulo para ver archivos
 var servidor = http.createServer(function(request, response) { // request: recibir
     if (request.method == 'GET') {
         var uri = url.parse(request.url).pathname;
-        console.log(uri);               // /doc/test2.json
+        console.log(uri);                               // /doc/test2.json
         if (fs.existsSync('.' + uri)) {                 // si existe ./doc/test2.json
             console.log('Encontrado....');
             var archivo = fs.readFileSync('.' + uri);   //  ./doc/test2.json
             response.writeHead(200, {'Content-Type': 'text/plain'});
-            response.end(archivo,'binary');
+            response.end(archivo, 'binary');
             return;
         } else {
             console.log('NO encontrado....');
